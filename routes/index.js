35e44b6bfbbf4017bda0413ab9c24e094ccbc1e9
Soezdemir23 <Soezdemir3423@gmail.com>
2022-03-17 07:42:44 -0600

    refactor(packages.json): added new scripts

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi, there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World",
+    user: "Amando",
+    added: new Date()
+  }
+];
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
 module.exports = router;
+
+router.get('/new', function(req, res, next) {
+  res.render('new', {title: 'New Post'} )
+});
+
